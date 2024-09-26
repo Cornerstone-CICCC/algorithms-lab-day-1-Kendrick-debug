af7d06d2278ae8809eef64ead75474c713c215cc
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 22:09:36 -0700

    1 completed

--- ex01.js
+++ ex01.js
@@ -1,7 +1,12 @@
 // Exercise 1: Write a function named 'sumPrices' that takes an array of product objects (each with a 'price' property) and returns the total sum of all prices.
 
 function sumPrices(products) {
-  // your code here
+  let sumPrices = 0
+
+  for (let i = 0; i < products.length; i++) {
+    sumPrices += products[i].price
+  } 
+  return sumPrices
 }
 
 console.log(sumPrices([{ price: 10 }, { price: 20 }, { price: 30 }])) // 60
