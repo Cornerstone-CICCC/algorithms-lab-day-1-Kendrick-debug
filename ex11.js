b9633a7e7fb08af5b906aa8d91bb7aa35222ce2c
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 22:15:07 -0700

    exercise 11 completed

--- ex11.js
+++ ex11.js
@@ -1,7 +1,17 @@
 // Exercise 11: Write a function named 'reverseWords' that takes a string of text and returns the string with the order of the words reversed.
 
+// function reverseWords(text) {
+//   const words = text.split(" ");
+// return words
+// }
+
+// console.log(reverseWords('The quick brown fox')) // 'fox brown quick The'
+
 function reverseWords(text) {
-  // your code here
+  const words = text.split(" "); // To Split into words this is how we do it. Remember!
+  words.reverse(); //  Reverse the array of words
+  return words.join(" ");
 }
 
-console.log(reverseWords('The quick brown fox')) // 'fox brown quick The'
+// Example usage
+console.log(reverseWords("The quick brown fox")); // 'fox brown quick The'
