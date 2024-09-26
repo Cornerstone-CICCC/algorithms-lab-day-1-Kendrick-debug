153a7a52b152f4aa8fc4d2f646dc2aee8af0b28d
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 22:12:25 -0700

    exercise 4 completed

--- ex04.js
+++ ex04.js
@@ -1,7 +1,15 @@
 // Exercise 4: Write a function named 'countOccurrences' that takes an array of strings and returns an object with the count of each string in the array.
 
 function countOccurrences(arr) {
-  // your code here
+  // Do a for loop with the arr.
+  // Return the counts of the array item/length or countOccurance
+  // The return type should look like an obect
+  // The object should look like {key : count}
+  let counts = {};
+  for (let i = 0; i < arr.length; i++) {
+    counts[arr[i]] = counts[arr[i]] ? counts[arr[i]] + 1 : 1;
+  }
+  return counts;
 }
 
-console.log(countOccurrences(['apple', 'banana', 'apple'])) // { apple: 2, banana: 1 }
+console.log(countOccurrences(["apple", "banana", "apple"])); // { apple: 2, banana: 1 }
