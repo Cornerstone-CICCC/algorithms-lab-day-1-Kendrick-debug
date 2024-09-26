14278729b618f909e3f145250857d86d627d5366
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-25 22:14:19 -0700

    exercise 8 completed

--- ex08.js
+++ ex08.js
@@ -1,7 +1,20 @@
 // Exercise 8: Write a function named 'longestWord' that takes a string of text and returns the longest word in the string.
 
 function longestWord(text) {
-  // your code here
-}
+  const words = text.split(" ");
 
-console.error.longestWord('The quick brown fox jumps over the lazy dog') // 'jumps'
+  return words.reduce((longest, current) => {
+    return current.length > longest.length ? current : longest;
+  }, "");
+}
+console.log(longestWord("The precident gave me a handshake"));
+
+// function longestWord(text) {
+//   const words = text.split(" "); // Step 3: Split into words
+
+//   return words.reduce((longest, current) => {
+//     return current.length > longest.length ? current : longest; // Step 6: Compare lengths
+//   }, "");
+// }
+
+// console.log(longestWord("The precident gave me a handshake")); // Output: 'jumps'
