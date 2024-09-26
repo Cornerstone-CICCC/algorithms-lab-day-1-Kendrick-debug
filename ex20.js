e1be507d3291f540580821e080666dffad747f56
Kendrick-debug <boahenep22@gmail.com>
2024-09-25 22:29:29 -0700

    Update ex20.js

--- ex20.js
+++ ex20.js
@@ -1,7 +1,1 @@
-// Exercise 20: Write a function named 'anagramGroups' that takes an array of strings and returns an array of arrays, grouping anagrams together.
 
-function anagramGroups(arr) {
-  // your code here
-}
-
-console.log(anagramGroups(['bat', 'tab', 'cat', 'act'])) // [['bat', 'tab'], ['cat', 'act']]
